4e02d133e74563daf8fd32f14ccfbf5c52ddb7fa
Nicholas J Humfrey <njh@aelius.com>
2006-06-28 18:28:28 +0000

    Changed rtpsend.pl to open pipe to sox, which makes it more usable

--- tools/rtpsend-pcmu.pl
+++ tools/rtpsend-pcmu.pl
@@ -1,37 +1,34 @@
 #!/usr/bin/perl
 #
-# Net::RTP example file
+# Send an audio file to specified address and port
+# using PCM u-law payload type (0)
 #
-# Send a file containing PCMU audio 
-# to specified address and port
-#
-# File can be generated using:
-# sox input.aiff -t raw -b -U -c 1 -r 8000 output.raw
+# Requires 'sox' command to help transcode the audio file
 #
 
 use Net::RTP;
 use Time::HiRes qw/ usleep /; 
 use strict;
 
-
+my $DEFAULT_PORT = 5004;	# Default RTP port
+my $DEFAULT_TTL = 2;		# Default Time-to-live
 my $PAYLOAD_TYPE = 0;		# u-law
 my $PAYLOAD_SIZE = 160;		# 160 samples per packet
 
 
-# Make STDOUT unbuffered
-$|=1;
-
-# Check the number of arguments
-if ($#ARGV != 2) {
-	print "usage: rtpsend.pl filename dest_addr dest_port\n";
-	exit;
-}
-
 # Get the command line parameters
-my ($filename, $address, $port ) = @ARGV;
+my ($filename, $address, $port, $ttl ) = @ARGV;
+usage() unless (defined $filename);
+usage() unless (defined $address);
+$port=$DEFAULT_PORT unless (defined $port);
+$ttl=$DEFAULT_TTL unless (defined $ttl);
+
 print "Input Filename: $filename\n";
 print "Remote Address: $address\n";
 print "Remote Port: $port\n";
+print "Multicast TTL: $ttl\n";
+print "Payload type: $PAYLOAD_TYPE\n";
+print "Payload size: $PAYLOAD_SIZE bytes\n";
 
 
 
@@ -41,14 +38,17 @@ my $rtp = new Net::RTP(
 		PeerAddr=>$address,
 );
 
+# Set the TTL
+$rtp->mcast_ttl( $ttl );
 
 # Create RTP packet
 my $packet = new Net::RTP::Packet();
 $packet->payload_type( $PAYLOAD_TYPE );
 
 
-# Open the input file
-open(PCMU, $filename) or die "Failed to open input file: $!";
+# Open the input file (via sox)
+open(PCMU, "sox '$filename' -t raw -b -U -c 1 -r 8000 - |") 
+or die "Failed to open input file: $!";
 
 my $data;
 while( my $read = read( PCMU, $data, $PAYLOAD_SIZE ) ) {
@@ -69,3 +69,7 @@ while( my $read = read( PCMU, $data, $PAYLOAD_SIZE ) ) {
 close( PCMU );
 
 
+sub usage {
+	print "usage: rtpsend-pcmu.pl <filename> <dest_addr> [<dest_port>] [<ttl>]\n";
+	exit -1;
+}
